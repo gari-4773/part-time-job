8a2ac3d1db8f7cf6fe65b394320a9945a581f84e
gari-4773 <garigari4773@icloud.com>
2021-09-28 13:20:54 +0900

    一度削除する

--- application/views/complete.php
+++
@@ -1,8 +0,0 @@
-<body>
-    <p class="text-center">支払いが完了しました。</p>
-    <p class="text-center">これ以降は送付メール記載の求職者様宛てのメールアドレスにて連絡をお願いします。</p>
-    <div class="text-center">
-        <a href="<?= base_url() ?>main/players">ホームに戻る</a>
-    </div>
-</body>
-</html>
